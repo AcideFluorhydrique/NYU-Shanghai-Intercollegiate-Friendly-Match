a538a2f356562c19f2a458f64ac70c523c099de2
Geo Yujia Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2024-05-05 18:57:26 +0800

    Update Basic Substring Structure.py

--- Basic Substring Structure.py
+++ Basic Substring Structure.py
@@ -22,47 +22,18 @@ A = []
 for i in range(T):
     n = int(input())
     si = input()
-
     B = list(map(int, si.split()))
-    #for j in range(n):
-        #si = int(input(""))
-        #2
-        # B.append(si)
-
     p = 0
     for k in range(n):
-        
-        # F = []
-        # p = 0
-        """
-        for l in B:
-            F = []
-            if l != B[k]:
-                C[k] = l
-                F.append(f(C))
-                fc = max(F)
-                output = fc^(int(l))
-                p += output
-        """
         F = []
         for l in range(1,1 + n):
             C = B.copy()
-            #F = []
             if l != C[k]:
                 C[k] = l
-                #print("****",f(C))
                 F.append(f(C))
-
         fc = max(F)
-        #print(fc)
         output = fc^(k+1)
         p += output
-
-
-
-    #for m in range(n)
-    #xor = output^
     A.append(p)
-
 for i in A:
     print(i)
